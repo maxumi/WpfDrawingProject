a44695e229db5ad0fcec1dc2b2fb6d08ebf27878
maxumi <maxjac64@gmail.com>
2023-09-01 12:35:51 +0200

    Add project files.

---
+++ WpfEndProject/Commands/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfEndProject.Commands
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action methodToExecute;
+        private Func<bool> canExecuteEvaluator;
+
+        public RelayCommand(Action methodToExecute, Func<bool> canExecute)
+        {
+            this.methodToExecute = methodToExecute;
+            this.canExecuteEvaluator = canExecute;
+        }
+
+        public RelayCommand(Action methodToExecute)
+        {
+            this.methodToExecute = methodToExecute;
+            this.canExecuteEvaluator = () => true;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object? parameter) => canExecuteEvaluator?.Invoke() ?? true;
+
+        public void Execute(object? parameter) => methodToExecute?.Invoke();
+    }
+}
---
+++ WpfEndProject/ViewModels/ToolbarViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfEndProject.Commands;
+
+namespace WpfEndProject.ViewModels
+{
+    internal class ToolbarViewModel : ViewModelBase
+    {
+        public ICommand SelectCommand { get; }
+        public ICommand DrawLineCommand { get; }
+
+        public ToolbarViewModel()
+        {
+            SelectCommand = new RelayCommand(ExecuteSelectCommand);
+            DrawLineCommand = new RelayCommand(ExecuteDrawLineCommand);
+
+        }
+
+        private void ExecuteSelectCommand()
+        {
+            // Implement logic for the Select button
+        }
+
+        private void ExecuteDrawLineCommand()
+        {
+            // Implement logic for the Draw Line button
+        }
+    }
+}
